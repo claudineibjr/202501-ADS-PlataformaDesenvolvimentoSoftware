bd7614a61462716521fe5b16484eeec08481144d
Claudinei B Jr <claudineibjr@hotmail.com>
2025-02-10 08:36:13 -0300

    Implementing exercise list 1 - Exercises 1 to 8

--- Lista1/Ex1/Program.cs
+++ Lista1/Ex1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// L1.1. Escreva um programa que receba o nome do aluno e no final, exiba uma saudação de boas vindas. (nível 1)
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Seja bem-vindo, {nome}");
---
+++ Lista1/Ex7/Program.cs
@@ -0,0 +1,8 @@
+﻿// 7. Incremente o programa anterior exibindo a média com 2 casas decimais. (nível 2)
+
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double resultado = (num1 + num2) / 2;
+
+Console.WriteLine($"{resultado:f2}");
