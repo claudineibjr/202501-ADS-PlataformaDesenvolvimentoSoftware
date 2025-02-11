813e049c575ea7e1084c00463e56f616a07682c4
Claudinei B Jr <claudineibjr@hotmail.com>
2025-02-11 09:57:38 -0300

    Implementing exercises 8 to 11

---
+++ Lista1/Ex10/Program.cs
@@ -0,0 +1,33 @@
+﻿// Incremente o programa anterior, de forma que sejam 3 produtos e 
+// seja exibido o valor total e também o total por produto. (nível 3)
+
+// Produto 1
+string produto1 = Console.ReadLine();
+decimal valor1 = Convert.ToDecimal(Console.ReadLine());
+int quantidade1 = Convert.ToInt32(Console.ReadLine());
+
+decimal valorTotal1 = valor1 * quantidade1;
+
+// Produto 2
+string produto2 = Console.ReadLine();
+decimal valor2 = Convert.ToDecimal(Console.ReadLine());
+int quantidade2 = Convert.ToInt32(Console.ReadLine());
+
+decimal valorTotal2 = valor2 * quantidade2;
+
+// Produto 3
+string produto3 = Console.ReadLine();
+decimal valor3 = Convert.ToDecimal(Console.ReadLine());
+int quantidade3 = Convert.ToInt32(Console.ReadLine());
+
+decimal valorTotal3 = valor3 * quantidade3;
+
+// Final
+decimal valorFinal = valorTotal1 + valorTotal2 + valorTotal3;
+
+string mensagem = @$"Total: R$ {valorFinal:f2}
+ - {quantidade1}x {produto1} - R$ {valorTotal1:f2}
+ - {quantidade2}x {produto2} - R$ {valorTotal2:f2}
+ - {quantidade3}x {produto3} - R$ {valorTotal3:f2}";
+
+Console.WriteLine(mensagem);
---
+++ Lista1/Ex11/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Escreva um programa que receba o valor total de um financiamento, o número
+total de parcelas e o número de parcelas já pagas. (nível 4) Com base nisso, informe ao usuário:
+Valor da parcela mensal
+Valor já pago
+Valor a pagar
+Valor total do financiamento
+Número de parcelas pagas
+Número de parcelas a pagar
+% já pago
+Ordem dos dados de entrada: Valor total do financiamento / Número total de parcelas / Número de parcelas pagas
+*/
+
+// Entrada
+decimal valorTotal = Convert.ToDecimal(Console.ReadLine());
+int numParcelas = Convert.ToInt32(Console.ReadLine());
+int numParcelasPagas = Convert.ToInt32(Console.ReadLine());
+
+// Cálculo
+decimal parcelaMensal = valorTotal / numParcelas;
+decimal valorPago = numParcelasPagas * parcelaMensal;
+decimal numParcelasAPagar = numParcelas - numParcelasPagas;
+decimal valorAPagar = numParcelasAPagar * parcelaMensal;
+decimal percentualPago = (decimal)(numParcelasPagas) / (decimal)(numParcelas) * 100;
+
+string mensagem = @$"Resumo do financiamento
+ - Parcela mensal: R$ {parcelaMensal:f2}
+ - Valor total pago: R$ {valorPago:f2} | Valor a pagar: R$ {valorAPagar:f2}
+ - Valor total: R$ {valorTotal:f2}
+ - Progresso: {numParcelasPagas} / {numParcelas} ({percentualPago:f2}%) de parcelas pagas - {numParcelasAPagar} parcelas a pagar";
+
+Console.WriteLine(mensagem);
