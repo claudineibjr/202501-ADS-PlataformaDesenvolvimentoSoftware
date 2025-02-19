f0fa21b2501f72ac2612d9b22257560611ebea90
Claudinei B Jr <claudineibjr@hotmail.com>
2025-02-18 21:27:57 -0300

    Including if example

---
+++ Exemplos/Exemplo05-IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿int idade = 20;
+
+bool éEmancipado = false;
+
+bool temGrana = true;
+
+bool temCarona = false;
+
+if ((idade >= 18 || éEmancipado) && temGrana) {
+  Console.WriteLine("Pode tirar CNH");
+} else if (temCarona) {
+  Console.WriteLine("Pode ir de carona");
+} else {
+  Console.WriteLine("Não pode tirar CNH");
+}
