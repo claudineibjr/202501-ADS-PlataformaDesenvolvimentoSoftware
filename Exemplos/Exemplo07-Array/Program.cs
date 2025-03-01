df08a2888135e9acf636dcaffc0ed8ede7355586
claudineibjr-unimar <claudineibjr@unimar.br>
2025-02-28 21:12:11 -0300

    Examples for array

---
+++ Exemplos/Exemplo07-Array/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numeros = new int[3];
+
+numeros[0] = 10;
+numeros[1] = 20;
+numeros[2] = numeros[0] * 3;
+
+Console.WriteLine(numeros[0]);
+Console.WriteLine(numeros[1]);
+Console.WriteLine(numeros[2]);
---
+++ Exemplos/Exemplo08-ArrayTamanhoDinamico/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite a quantidade de alunos: ");
+int numAlunos = Convert.ToInt32(Console.ReadLine());
+
+string[] alunos = new string[numAlunos];
+
+Console.WriteLine($"Tamanho do array: {alunos.Length}");
