dc0379d0dedc2630d888ca553ff55f30717ace0e
claudineibjr-unimar <claudineibjr@unimar.br>
2025-03-07 20:00:16 -0300

    Including exercises 03 e 05 from List 04

---
+++ Soluções/Lista04/Ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿/*3. (Exibição do nome de trás para frente) Implemente um programa
+que leia o nome do usuário e exiba este nome escrito de trás para
+frente.*/
+
+string nome = Console.ReadLine();
+
+for (int iCount = nome.Length -1; iCount >= 0; iCount--)
+{
+    Console.Write(nome[iCount]);
+}
---
+++ Soluções/Lista04/Ex05/Program.cs
@@ -0,0 +1,33 @@
+﻿/*5. (Menor e maior idade entre os alunos) Incremente o programa
+Média de idade - Número de alunos fixo (5 alunos), de forma a identificar a
+menor e a maior idade entre os alunos.*/
+
+int[] idades = new int[3];
+int somaIdades = 0;
+
+int maior = 0, menor = 0;
+
+for (int iCount = 0; iCount < idades.Length; iCount++)
+{
+    int idade = Convert.ToInt32(Console.ReadLine());
+    idades[iCount] = idade;
+    somaIdades += idade;
+
+    if (iCount == 0)
+    {
+        menor = idade;
+        maior = idade;
+    }
+
+    if (idade > maior)
+    {
+        maior = idade;
+    }
+    if (idade < menor)
+    {
+        menor = idade;
+    }
+}
+
+double media = somaIdades / idades.Length;
+Console.WriteLine($"Média: {media:f2} | Menor: {menor} | Maior: {maior}");
