11280a413229cfaf377e62e4187c7c31203f0bc8
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-14 20:30:37 -0300

    Exercise 7.2

---
+++ Soluções/Lista07/Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        static void MostraArray(int[] idades, string ordenacao = "C")
+        {
+            if (ordenacao != "C" && ordenacao != "D")
+            {
+                Console.WriteLine("Opção de ordenação inválido");
+                return;
+            }
+
+            Array.Sort(idades);
+
+            if (ordenacao == "D")
+            {
+                Array.Reverse(idades);
+            }
+
+            foreach (int idade in idades)
+            {
+                Console.WriteLine(idade);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] idades = new int[3];
+
+            Random rnd = new Random();
+            for (int iCount = 0; iCount < idades.Length; iCount++)
+            {
+                idades[iCount] = rnd.Next(0, 101);
+            }
+
+            Console.Write("Ordenação - C / D: ");
+            string ordenacao = Console.ReadLine();
+
+            MostraArray(idades, ordenacao);
+
+            Console.WriteLine("Fim!");
+        }
+    }
+}
