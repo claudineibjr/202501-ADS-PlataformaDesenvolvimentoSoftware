d522597170afb8413457018c1b3d2c0aa16f7617
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-18 21:29:00 -0300

    List7.Ex4

---
+++ Soluções/Lista07/Ex04/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex04
+{
+    internal class Program
+    {
+
+        static int ContaPalavas(string frase)
+        {
+            string[] palavras = frase.Split(" ");
+            return palavras.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            int palavras = ContaPalavas(frase);
+
+            Console.WriteLine($"Palavras: {palavras}");
+        }
+    }
+}
