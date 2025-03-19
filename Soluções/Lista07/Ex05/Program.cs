317ca5ccd4ebe200c9a7a6f4d2492d7493fabd4f
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-18 21:31:52 -0300

    List7.Ex5

---
+++ Soluções/Lista07/Ex05/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex05
+{
+    internal class Program
+    {
+        /*
+            5. (Contagem de vogais com funções) Implemente um programa que conte quantas vezes cada uma
+            das vogais aparecem em uma determinada frase. Para isso, crie uma função que recebe a
+            frase a verificar e a letra a verificar, e então devolva o número de vezes que aquela letra aparece na frase.
+         */
+
+        static int ContaLetra(string palavra, char letraAContar)
+        {
+            int contador = 0;
+            foreach (char letra in palavra)
+            {
+                if (letra == letraAContar)
+                {
+                    contador++;
+                }
+            }
+
+            return contador;
+        }
+
+        static void Main(string[] args)
+        {
+            string palavra = Console.ReadLine();
+
+            int letrasA = ContaLetra(palavra, 'A');
+            int letrasE = ContaLetra(palavra, 'E');
+            int letrasI = ContaLetra(palavra, 'I');
+            int letrasO = ContaLetra(palavra, 'O');
+            int letrasU = ContaLetra(palavra, 'U');
+
+            Console.WriteLine($"A: {letrasA}");
+            Console.WriteLine($"E: {letrasE}");
+            Console.WriteLine($"I: {letrasI}");
+            Console.WriteLine($"O: {letrasO}");
+            Console.WriteLine($"U: {letrasU}");
+
+
+        }
+    }
+}
