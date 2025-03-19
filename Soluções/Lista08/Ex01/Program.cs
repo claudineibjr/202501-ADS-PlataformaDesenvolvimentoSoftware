8ce19993c2f31392726676c2668050984957afa3
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-18 21:47:54 -0300

    List8.Ex01

---
+++ Soluções/Lista08/Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static double LerNota()
+        {
+            Console.Write("Digita a nota: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            if (nota < 0 || nota > 10)
+            {
+                throw new Exception("Nota inválida. A nota deve ser entre 0 e 10");
+            }
+
+            return nota;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                double nota1 = LerNota();
+                double nota2 = LerNota();
+
+                double media = (nota1 + nota2) / 2;
+                Console.Write($"Nota final: {media}");
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao calcular a nota.\n{ex.Message}");
+            }
+        }
+    }
+}
