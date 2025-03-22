fa538eb0cb938638560bfff34396fadc09e709fb
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-21 22:41:31 -0300

    List08 - Ex02 -- Ex05

---
+++ Soluções/Lista08/Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        static double LerNota()
+        {
+            do
+            {
+                try
+                {
+                    Console.Write("Digite a nota: ");
+                    double nota = Convert.ToDouble(Console.ReadLine());
+
+                    if (nota < 0 || nota > 10)
+                    {
+                        throw new Exception("Nota inválida. A nota deve ser entre 0 e 10");
+                    }
+
+                    return nota;
+                }
+                catch
+                {
+                    Console.WriteLine("Nota inválida.");
+                }
+            } while (true);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                double nota1 = LerNota();
+                double nota2 = LerNota();
+
+                double media = (nota1 + nota2) / 2;
+                Console.Write($"Nota final: {media}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao calcular a nota.\n{ex.Message}");
+            }
+        }
+    }
+}
---
+++ Soluções/Lista08/Ex03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Ex03
+{
+    internal class Program
+    {
+        static string FormataComoMoeda(decimal valor)
+        {
+            return valor.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+                3. (Caixa de supermercado) Implemente um programa que crie uma
+                lista e armazene o preço de produtos passados no caixa do
+                supermercado. O programa deve continuar lendo o preço dos produtos
+                até que o preço digitado seja -1. Exiba o valor final da compra e a
+                quantidade de produtos.
+             */
+
+            List<decimal> precos = new List<decimal>();
+
+            decimal novoPreco = -1;
+
+            do
+            {
+                Console.Write("Digite o preço do produto (-1 para sair): ");
+                novoPreco = Convert.ToDecimal(Console.ReadLine());
+                if (novoPreco != -1)
+                {
+                    precos.Add(novoPreco);
+                }
+
+            } while (novoPreco != -1);
+
+            Console.WriteLine($"Valor final: {FormataComoMoeda(precos.Sum())}");
+        }
+    }
+}
