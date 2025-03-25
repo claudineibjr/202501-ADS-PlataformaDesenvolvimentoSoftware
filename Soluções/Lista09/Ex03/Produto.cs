ddd37d5f367b0bc3246bcbf2b03a0ed408be2573
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-24 22:05:15 -0300

    List09.Ex03

---
+++ Soluções/Lista09/Ex03/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Ex03
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public string PrecoFormatadoParaMoeda
+        {
+            get
+            {
+                return this.Preco.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+            }
+        }
+
+        public Produto(string Nome, decimal Preco, int QuantidadeDisponivel)
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.QuantidadeDisponivel = QuantidadeDisponivel;
+        }
+
+        public string ExibirInformacoes()
+        {
+            return $"{this.Nome} {PrecoFormatadoParaMoeda} ({this.QuantidadeDisponivel})";
+        }
+    }
+}
---
+++ Soluções/Lista09/Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o preço do produto: ");
+            decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a quantidade em estoque: ");
+            int estoque = Convert.ToInt32(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, estoque);
+            Console.WriteLine(produto.ExibirInformacoes());
+        }
+    }
+}
