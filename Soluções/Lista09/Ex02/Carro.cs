ef4763cfd41de7a1c12aa1c169a5693cb8212c72
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-03-24 21:50:36 -0300

    List9.Ex02

---
+++ Soluções/Lista09/Ex02/Carro.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex02
+{
+    internal class Carro
+    {
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+
+        public Carro(string modelo, int ano)
+        {
+            this.Modelo = modelo;
+            this.Ano = ano;
+        }
+
+        public string ObterInformacoes()
+        {
+            return $"{this.Modelo} ({this.Ano})";
+        }
+    }
+}
