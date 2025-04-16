ae90eff41b8024d8e0e1d31db223fff226e2ea6f
Claudinei BJr (UNIMAR) <claudineibjr@unimar.br>
2025-04-15 22:42:20 -0300

    Exemplo23-0-BancoDeDados

---
+++ Exemplos/Exemplo23-0-BancoDeDados/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp8
+{
+    public class Pessoa
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        private Pessoa()
+        {            }
+    }
+}
---
+++ Exemplos/Exemplo23-0-BancoDeDados/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoasDbContext db = new PessoasDbContext();
+
+            Console.WriteLine("Pessoas: ");
+            foreach(Pessoa pessoa in db.Pessoas.ToList())
+            {
+                Console.WriteLine($"{pessoa.Nome} ({pessoa.Idade})");
+            }
+        }
+    }
+}
