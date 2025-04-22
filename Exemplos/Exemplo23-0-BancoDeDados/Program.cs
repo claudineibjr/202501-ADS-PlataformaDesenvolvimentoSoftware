575af4c7145a08aa28a8e2f4e7648e673aa230dd
ClaudineiBJr <claudineibjr@unimar.br>
2025-04-22 20:10:03 -0300

    Improvements for creation/reading on Exemplo23-0-BancoDados

--- Exemplos/Exemplo23-0-BancoDeDados/Program.cs
+++ Exemplos/Exemplo23-0-BancoDeDados/Program.cs
@@ -2,15 +2,39 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            PessoasDbContext db = new PessoasDbContext();
 
+        static PessoasDbContext db = new PessoasDbContext();
+
+        static void ExibirPessoas(int idadeMinima)
+        {
             Console.WriteLine("Pessoas: ");
-            foreach(Pessoa pessoa in db.Pessoas.ToList())
+            IEnumerable<Pessoa> pessoasOrdenadasPorIdade = db.Pessoas
+                .OrderByDescending(p => p.Idade)
+                .Where(p => p.Idade >= idadeMinima);
+            foreach (Pessoa pessoa in pessoasOrdenadasPorIdade)
             {
                 Console.WriteLine($"{pessoa.Nome} ({pessoa.Idade})");
             }
+        }
+
+        static void CriarPessoa()
+        {
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Pessoa novaPessoa = new Pessoa(nome, idade);
+
+            db.Pessoas.Add(novaPessoa);
+            db.SaveChanges();
+        }
+
+        static void Main(string[] args)
+        {
+            CriarPessoa();
+            ExibirPessoas(0);
         }
     }
 }
