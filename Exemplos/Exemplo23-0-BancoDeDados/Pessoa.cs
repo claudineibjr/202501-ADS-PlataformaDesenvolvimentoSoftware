943082582fcec05352e51bd9c9b19cd8ee3e4965
ClaudineiBJr-Unimar <claudineibjr@unimar.br>
2025-04-25 19:32:21 -0300

    Removing database implementation

--- Exemplos/Exemplo23-0-BancoDeDados/Pessoa.cs
+++ Exemplos/Exemplo23-0-BancoDeDados/Pessoa.cs
@@ -2,7 +2,6 @@
 {
     public class Pessoa
     {
-        public string Id { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Email { get; set; }
