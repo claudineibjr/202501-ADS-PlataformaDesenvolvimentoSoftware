b869afe40311fb48351865f7ecdba7528270bae9
Claudinei B Jr <claudineibjr@hotmail.com>
2025-04-29 20:31:17 -0300

    DbContext creation

---
+++ Solucoes/S11-Projeto11/S11ProjetoPedidos/PedidosManagerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace S11ProjetoPedidos;
+
+public partial class PedidosManagerDbContext : DbContext
+{
+    public PedidosManagerDbContext()
+    {
+    }
+
+    public PedidosManagerDbContext(DbContextOptions<PedidosManagerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user id=root;password=garrafa;database=db_pedidos_manager", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
