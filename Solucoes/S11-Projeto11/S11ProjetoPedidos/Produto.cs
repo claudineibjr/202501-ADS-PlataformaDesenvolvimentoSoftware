bd84435f87f5b1eae55ee02be0945a966929e609
Claudinei B Jr <claudineibjr@hotmail.com>
2025-04-29 20:29:11 -0300

    Improvements on Produto class to adapt it for EntityFramework

--- Solucoes/S11-Projeto11/S11ProjetoPedidos/Produto.cs
+++ Solucoes/S11-Projeto11/S11ProjetoPedidos/Produto.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace S11ProjetoPedidos;
 
 public class Produto
 {
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+  [Key]
+  public string Id { get; set; }
   public string Nome { get; set; }
   public decimal Preco { get; set; }
 
@@ -12,4 +17,6 @@ public class Produto
     this.Nome = nome;
     this.Preco = preco;    
   }
+
+  private Produto() { }
 }
