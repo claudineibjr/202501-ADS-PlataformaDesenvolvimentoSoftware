a22c008582ab471fa0a1d2f4c6158654fa92f8e0
Claudinei B Jr <claudineibjr@hotmail.com>
2025-04-29 22:34:08 -0300

    Implementing pedidos creation

--- Solucoes/S11-Projeto11/S11ProjetoPedidos/Program.cs
+++ Solucoes/S11-Projeto11/S11ProjetoPedidos/Program.cs
@@ -24,15 +24,21 @@ public class Program
     db.SaveChanges();
   }
 
+  static IEnumerable<Produto> GetProdutos() {
+    IEnumerable<Produto> produtosOrdenadosPorNome = db.Produtos.OrderBy(p => p.Nome);
+
+    return produtosOrdenadosPorNome;
+  }
+
   static void ListarProdutos(bool exibirTitulo = true) {
     if (exibirTitulo) {
       Console.WriteLine("Produtos: ");
     }
     
-    IEnumerable<Produto> produtosOrdenadosPorNome = db.Produtos.OrderBy(p => p.Nome);
+    IEnumerable<Produto> produtos = GetProdutos();    
 
-    for (int iCount = 0; iCount < produtosOrdenadosPorNome.Count(); iCount++) {
-      Produto produto = produtosOrdenadosPorNome.ElementAt(iCount);
+    for (int iCount = 0; iCount < produtos.Count(); iCount++) {
+      Produto produto = produtos.ElementAt(iCount);
 
       Console.WriteLine($"{iCount + 1} - {produto.Nome} {produto.Preco.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
     }
@@ -86,15 +92,18 @@ public class Program
     db.SaveChanges();
   }
 
+  static IEnumerable<Cliente> GetClientes() {
+    return db.Clientes.OrderBy(p => p.Nome);
+  }
+
   static void ListarClientes(bool exibirTitulo = true) {
     if (exibirTitulo) {
       Console.WriteLine("Clientes: ");
     }
 
-    IEnumerable<Cliente> clientesOrdenadosPorNome = db.Clientes.OrderBy(p => p.Nome);
-
-    for (int iCount = 0; iCount < clientesOrdenadosPorNome.Count(); iCount++) {
-      Cliente cliente = clientesOrdenadosPorNome.ElementAt(iCount);
+    IEnumerable<Cliente> clientes = GetClientes();
+    for (int iCount = 0; iCount < clientes.Count(); iCount++) {
+      Cliente cliente = clientes.ElementAt(iCount);
 
       Console.WriteLine($"{iCount + 1} - {cliente.Nome} {cliente.CPF}");
     }
@@ -132,6 +141,92 @@ public class Program
     } while (opcao != "0");
   }
 
+  static Cliente SelecionarCliente() {
+    Console.WriteLine("Selecione o cliente");
+    ListarClientes(exibirTitulo: false);
+
+    Console.Write("Cliente selecionado: ");
+    int indiceSelecionado = Convert.ToInt32(Console.ReadLine());
+
+    Cliente cliente = GetClientes().ElementAt(indiceSelecionado - 1);
+
+    return cliente;
+  }
+
+  static List<Produto> SelecionarProdutos() {
+    List<Produto> produtosSelecionados = new List<Produto>();
+
+    int indiceSelecionado = 0;
+
+    do {
+      Console.WriteLine("Selecione os produtos");
+      ListarProdutos(exibirTitulo: false);
+      Console.WriteLine("0 - Terminar seleção");
+
+      Console.Write("Produto selecionado: ");
+      indiceSelecionado = Convert.ToInt32(Console.ReadLine());
+
+      if (indiceSelecionado > 0) {
+        Produto produto = GetProdutos().ElementAt(indiceSelecionado - 1);
+
+        produtosSelecionados.Add(produto);
+      }
+    } while (indiceSelecionado > 0);
+
+    return produtosSelecionados;
+  }
+
+  static void CadastrarPedido() {
+    Console.WriteLine("Cadastro de pedido: ");
+
+    Cliente cliente = SelecionarCliente();
+
+    List<Produto> produtos = SelecionarProdutos();
+
+    Pedido pedido = new Pedido(cliente, produtos);
+    Pedidos.Add(pedido);
+  }
+
+  static void ListarPedidos() {
+    Console.WriteLine("Pedidos: ");
+
+    IEnumerable<Pedido> pedidosOrdenadosPorData = Pedidos.OrderByDescending(p => p.Data);
+
+    for (int iCount = 0; iCount < pedidosOrdenadosPorData.Count(); iCount++) {
+      Pedido pedido = pedidosOrdenadosPorData.ElementAt(iCount);
+
+      Console.WriteLine($"{iCount + 1} - {pedido.Cliente.Nome} {pedido.ValorTotal.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
+    }
+  }
+
+  static void MenuPedido() {
+    string opcao = "0";
+
+    string menu = @"
+  Menu Pedidos
+    1 - Cadastrar
+    2 - Listar
+    0 - Sair";
+
+    do
+    {
+      Console.WriteLine(menu);
+      opcao = Console.ReadLine();
+
+      switch (opcao)
+      {
+        case "1":
+          CadastrarPedido();
+          break;
+
+        case "2":
+          ListarPedidos();
+          break;
+      }
+
+    } while (opcao != "0");
+  }
+
   public static void Main(string[] args) {
     string opcao = "0";
 
@@ -139,6 +234,7 @@ public class Program
 Menu Principal
   1 - Produtos
   2 - Clientes
+  3 - Pedidos
   0 - Sair";
 
     do
@@ -154,6 +250,10 @@ Menu Principal
 
         case "2":
           MenuCliente();
+          break;
+
+        case "3":
+          MenuPedido();
           break;
       }
 
