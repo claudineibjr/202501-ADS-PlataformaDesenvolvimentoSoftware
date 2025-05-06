a744a5a9d3db96263c8b9bcdb1f479dca37d72e9
Claudinei B Jr <claudineibjr@hotmail.com>
2025-05-06 07:51:49 -0300

    Including PedidosManagerDbContext as injection dependency on Program.cs

--- Exemplos/Exemplo25_API_Pedidos/Program.cs
+++ Exemplos/Exemplo25_API_Pedidos/Program.cs
@@ -1,3 +1,4 @@
+using Exemplo25_API_Pedidos.Database;
 
 namespace Exemplo25_API_Pedidos;
 
@@ -13,6 +14,9 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        // Configuração do Banco de Dados
+        builder.Services.AddDbContext<PedidosManagerDbContext>();
 
         var app = builder.Build();
 
