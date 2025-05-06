c85797375c60aa180a4ad5f611979c9eec2363db
Claudinei B Jr <claudineibjr@hotmail.com>
2025-05-06 08:04:12 -0300

    Implemeting GET api/Produto

---
+++ Exemplos/Exemplo25_API_Pedidos/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+using Exemplo25_API_Pedidos.Database;
+using Exemplo25_API_Pedidos.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exemplo25_API_Pedidos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+      private PedidosManagerDbContext dbContext;
+
+      public ProdutoController(PedidosManagerDbContext dbContext) {
+        this.dbContext = dbContext;
+      }
+
+      [HttpGet]
+      public ActionResult<IEnumerable<Produto>> GetProdutos() {
+        IQueryable<Produto> produtos = 
+          dbContext
+          .Produtos
+          .AsQueryable();
+
+        produtos = produtos.OrderBy(produto => produto.Nome);
+
+        return Ok(produtos);
+      }
+    }
+}
