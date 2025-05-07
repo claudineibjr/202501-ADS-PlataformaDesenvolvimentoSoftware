960f21bf4bc84d7b89e4ad881ff87aa62d72a1a4
ClaudineiBJr <claudineibjr@hotmail.com>
2025-05-06 22:38:25 -0300

    Implementing Exemplo25_0_API_Pedidos

---
+++ Exemplos/Exemplo25_0_API_Pedidos/Controllers/ProdutosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private static List<Produto> produtos = new List<Produto>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> GetProdutos()
+        {
+            return Ok(produtos);
+        }
+
+        [HttpPost]
+        public ActionResult<Produto> CreateProduto(Produto novoProduto)
+        {
+            produtos.Add(novoProduto);
+
+            return CreatedAtAction(nameof(CreateProduto), novoProduto);
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateProduto(string id, Produto produtoAAtualizar)
+        {
+            Produto? produtoEncontrado =
+                produtos
+                .FirstOrDefault(p => p.Id == id);
+
+            if (produtoEncontrado == null)
+            {
+                return NotFound();
+            }
+
+            produtoEncontrado.Nome = produtoAAtualizar.Nome;
+            produtoEncontrado.Preco = produtoAAtualizar.Preco;
+
+            return NoContent();
+
+        }
+    }
+}
---
+++ Exemplos/Exemplo25_0_API_Pedidos/Model/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Model
+{
+    public class Produto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public Produto(string nome, decimal preco)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
