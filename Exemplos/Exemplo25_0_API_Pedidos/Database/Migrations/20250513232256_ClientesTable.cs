72145ef73996b7acb44683d893ffe31ff8b4f41f
ClaudineiBJr <claudineibjr@hotmail.com>
2025-05-13 20:33:06 -0300

    Implementing Clientes CRUD

---
+++ Exemplos/Exemplo25_0_API_Pedidos/Database/Migrations/20250513232256_ClientesTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exemplo25_0_API_Pedidos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Nome = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CPF = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
---
+++ Exemplos/Exemplo25_0_API_Pedidos/Model/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exemplo25_0_API_Pedidos.Model
+{
+    public class Cliente
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+
+        public Cliente(string nome, string email, string cpf)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.CPF = cpf;
+        }
+
+        private Cliente()
+        {
+            
+        }
+    }
+}
