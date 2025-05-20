9d5ffd6ca76068f2522d580ea329eb9c953a3902
Claudinei B Jr <claudineibjr@hotmail.com>
2025-05-20 20:08:20 -0300

    Reapply "Implementing the usage of the bearer token on Swagger"
    This reverts commit 31d249762130b0a372ec31e64b2dfcf148c3900b.

--- Exemplos/Exemplo25_0_API_Pedidos/Program.cs
+++ Exemplos/Exemplo25_0_API_Pedidos/Program.cs
@@ -20,7 +20,32 @@ namespace WebApplication1
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Pedidos", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             builder.Services.AddDbContext<PedidosDbContext>();
 
